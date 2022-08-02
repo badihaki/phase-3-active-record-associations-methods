f335339382d574894d76ca156f00b046ebe82151
Badi-Haki Martin <jojackblack@gmail.com>
2022-08-01 22:04:35 -0400

    commit tree c6eac67494e95a82326619a579353a0bb6579fe8

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,16 +1,26 @@
+require "pry"
+
 class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artist_names = []
+    for performer in self.artists.all do
+      artist_names << performer.name
+      # binding.pry
+    end
+    artist_names
   end
 end
